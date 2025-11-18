c23a12f659f42e5a0ca3b109066c93db4683cda4
ephesians0611 <godsgurrl2391@gmail.com>
2025-11-18 08:34:01 -0800

    changed the HTML for the site.

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,9 @@
+from django.http import HttpResponse
+
+def post_list(request):
+    return HttpResponse("Hello, world! This is the blog homepage.")
 from django.shortcuts import render
 
 # Create your views here.
+def post_list(request):
+    return render(request, 'blog/post_list.html', {})
